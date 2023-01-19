b9ed0516ab99ec66ca1a1ec6d86ea8c4aaae3a28
ErpanOmer <354073823@qq.com>
2023-01-19 16:56:34 +0800

    feature: 提交toxcode

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Background>
+    <Center/>
+  </Background>
+</template>
+
+<script setup lang="ts">
+import Background from '@/components/Background.vue'
+import Center from '@/components/Center.vue'
+
+</script>
+
+<style scoped>
+
+</style>
